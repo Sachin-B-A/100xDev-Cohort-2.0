54631abaae90d705471094179650214c3472b206
Sachin B A <140411364+Sachin-B-A@users.noreply.github.com>
2024-09-08 22:39:34 +0530

    Add files via upload

---
+++ http_server.js
@@ -0,0 +1,26 @@
+//We are writing http server here using lib called express
+const express = require("express");
+const bodyParser = require("body-parser");
+const headParser = require("head-parser");
+const app = express();
+const port = 3000;
+//
+//or
+// const port= process.env.PORT
+
+app.use(bodyParser.json());
+// app.use(headParser.json);
+//res gives to function json and senf to respond to req.
+app.get("/conversations", (req, res) => {
+    console.log(req.body);// We have to use bodyparser to work this out
+    console.log(req.headers);
+    console.log(req.headers["Authorization"]);
+    res.send({
+        Name: "Sachin",
+        age:20
+    });
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
+});
