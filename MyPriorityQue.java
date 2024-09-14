d071311d9de58b3a8f664016b85320bdd34be51d
Sachin B A <140411364+Sachin-B-A@users.noreply.github.com>
2024-09-14 21:44:13 +0530

    Add files via upload

---
+++ MyPriorityQue.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class MyPriorityQue {
+    public static void main(String[] args) {
+        // Stores element according to priority
+        // Whenever ask for peak it shows up smallest element
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.add(30);
+        queue.add(10);
+        queue.add(20);
+        System.out.println("Priority Queue: " + queue);
+        System.out.println(queue.);
+    }
+    
+}
